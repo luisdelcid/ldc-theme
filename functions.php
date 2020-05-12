ad801ab1eafa4a94bed49253fa46566358564d4b
Luis del Cid <luisdelcid@gmail.com>
2020-05-12 08:32:32 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,5 +2,5 @@
 
 // Actions
 add_action('wp_enqueue_scripts', function(){
-    wp_enqueue_style('ldc-theme', get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_style('ldc-theme', get_stylesheet_uri(), [], '1.0');
 }, 1000);
